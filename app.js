1ff2eaab55e99f6258a7b2f5bb7ff53bd16fac5f
Enryahhh <linggaj28@gmail.com>
2022-04-14 15:16:50 +0700

    add basic post req

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 // const routes = require('./routes');
 const app = express();
 
-const server = http.createServer();
+app.use(bodyParser.urlencoded({extended:false}));
+
+// app.use('/',(req,res,next)=>{
+// 	console.log('always running');
+// 	next();
+// })
+
+app.use('/add-user',(req,res,next)=>{
+	res.send(`<form action="/user" method="POST">
+			<input type="text" name="name" id="">
+			<button>Send</button>
+		</form>`)
+})
+
+app.post('/user',(req,res,next)=>{
+	console.log(req.body);
+	res.redirect('/users');
+})
+
+
+app.use('/users',(req,res,next)=>{
+	console.log('tes middleware users');
+	res.send('<h1>Users Data</h1>');
+});
+
+app.use('/',(req,res,next)=>{
+	console.log('index middleware');
+	res.send('<h1>Hello From Express</h1>')
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
